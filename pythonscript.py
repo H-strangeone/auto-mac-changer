caa3962dfef601867d8831099f653fadeebc102c
H-strangeone <hiteshgitman@gmail.com>
2024-01-22 12:06:03 -0800

    i

--- pythonscript.py
+++ pythonscript.py
@@ -1,6 +1,6 @@
 import subprocess
 
 subprocess.call("ifconfig", shell=True)
-subprocess.call("ifconfig wlp0s20f3 down", shell=True)
-subprocess.call("ifconfig wlp0s20f3 hw ether 00:11:22:33:44:55", shell= True)
-subprocess.call("ifconfig wlp0s20f3 up",  shell=True)
+subprocess.call("sudo ifconfig wlp0s20f3 down", shell=True)
+subprocess.call("sudo ifconfig wlp0s20f3 hw ether 00:11:22:33:44:55", shell= True)
+subprocess.call("sudo ifconfig wlp0s20f3 up",  shell=True)
