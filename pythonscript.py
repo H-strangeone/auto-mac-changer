ce76353fedfe09099c50044b92195d4b98a88a36
H-strangeone <hiteshgitman@gmail.com>
2024-02-10 14:46:54 -0800

    i

--- pythonscript.py
+++ pythonscript.py
@@ -1,6 +1,11 @@
 import subprocess
-
+import optparse
+parser=optparse.OptionParser()
+parser.add_option("-i","--interface", dest="interface", help="Interface to change its MAC address")
+parser.parse_args
+interface=input("interface  =")
+new_mac=input("mac  address =")
 subprocess.call("ifconfig", shell=True)
-subprocess.call("sudo ifconfig wlp0s20f3 down", shell=True)
-subprocess.call("sudo ifconfig wlp0s20f3 hw ether 00:11:22:33:44:55", shell= True)
-subprocess.call("sudo ifconfig wlp0s20f3 up",  shell=True)
+subprocess.call("sudo ifconfig", interface ,"down", shell=True)
+subprocess.call("sudo ifconfig", interface,"hw ether", new_mac, shell= True)
+subprocess.call("sudo ifconfig", interface, "up",  shell=True)
