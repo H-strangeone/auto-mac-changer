91b346b6429783cd32f98ae624c17218d34b58d0
H-strangeone <hiteshgitman@gmail.com>
2024-02-14 12:23:03 -0800

    i

--- pythonscript.py
+++ pythonscript.py
@@ -8,9 +8,9 @@ parser.add_option("-m", "--mac", dest="new_mac", help="its MAC address")
 
 (options, arguments)=parser.parse_args()
 
-interface=options.interface
-new_mac=options.new_mac
+def change_mac(interface, new_mac):
+    subprocess.call(["sudo ifconfig "+interface+" down"], shell= True)#don not use , cause fir wo list ke lements samjhega har ek cheez ko unko concat karne ki jagah you can either use --thing or -thing both will work fine like --interface -i
+    subprocess.call(["sudo ifconfig "+interface+" hw "+"ether "+new_mac], shell=True)
+    subprocess.call(["sudo ifconfig "+interface+" up"],shell=True)
 
-subprocess.call(["sudo ifconfig "+interface+" down"], shell= True)#don not use , cause fir wo list ke lements samjhega har ek cheez ko unko concat karne ki jagah
-subprocess.call(["sudo ifconfig "+interface+" hw "+"ether "+new_mac], shell=True)
-subprocess.call(["sudo ifconfig "+interface+" up"],shell=True)
+change_mac(options.interface ,options.new_mac)
